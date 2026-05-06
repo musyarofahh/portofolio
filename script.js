26fe2497594b56418ae6df291cea74004a60c5d7
Musya <musyarofahh93@gmail.com>
2026-05-06 11:17:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
-// Smooth scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href'))
-            .scrollIntoView({
-                behavior: 'smooth'
-            });
+function showSection(id) {
+    document.querySelectorAll("section").forEach(sec => {
+        sec.classList.remove("active");
     });
-});
 
-// Navbar shadow on scroll
-window.addEventListener('scroll', function () {
-    const nav = document.querySelector('nav');
-    if (window.scrollY > 50) {
-        nav.style.boxShadow = "0 2px 10px rgba(0,0,0,0.2)";
-    } else {
-        nav.style.boxShadow = "none";
-    }
-});
+    document.getElementById(id).classList.add("active");
+}
