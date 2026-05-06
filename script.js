444f499a21a4140b992cfffa67f5aa5aac81146b
Musya <musyarofahh93@gmail.com>
2026-05-06 11:09:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Website Musyarofah siap jalan!");
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href'))
+            .scrollIntoView({
+                behavior: 'smooth'
+            });
+    });
+});
+
+// Navbar shadow on scroll
+window.addEventListener('scroll', function () {
+    const nav = document.querySelector('nav');
+    if (window.scrollY > 50) {
+        nav.style.boxShadow = "0 2px 10px rgba(0,0,0,0.2)";
+    } else {
+        nav.style.boxShadow = "none";
+    }
 });
