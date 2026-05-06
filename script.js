0185202d647ddc23d7498c5861471a235c430ac3
Musya <musyarofahh93@gmail.com>
2026-05-06 15:42:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ function showSection(id) {
     document.querySelectorAll("section").forEach(sec => {
         sec.classList.remove("active");
     });
-
     document.getElementById(id).classList.add("active");
+}
+
+// DETAIL TOGGLE
+function showDetail(id) {
+    document.querySelectorAll(".detail").forEach(d => {
+        d.style.display = "none";
+    });
+
+    document.getElementById(id).style.display = "block";
 }
